cf8898d64ef2fa7cc6f102718d430307d512d436
Mohammed-S-Adel <140682947+Mohammed-S-Adel@users.noreply.github.com>
2023-07-29 07:49:39 -0700

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -17,5 +17,15 @@ public class DemoDay2 {
 		System.out.println(a - b);
 	}
 
+	//Yasir
+	public static void divide(int a , int b) {
+		System.out.println(a / b);
+	}
+
+	//add method
+	public static void add (int a , int b , int c) {
+		System.out.println(a + b + c);
+	}
+
 
 }
